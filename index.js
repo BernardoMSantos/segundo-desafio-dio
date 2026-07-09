15b4ff5bfa8e1688f41f8afa80addf43dfe851d8
BernardoMSantos <moreirasantos11ms@outlook.com.br>
2026-07-09 13:52:12 -0300

    Primeira versão do desafio

---
+++ index.js
@@ -0,0 +1,41 @@
+let patidasVencidas = 9
+let partidasPerdidas 
+let rank
+let saldoDeVitorias = visualizarPartidas(patidasVencidas , partidasPerdidas)
+
+function visualizarPartidas(vitorias , derrotas = 0) {
+    let resultado = vitorias - derrotas
+    return resultado
+}
+
+while(saldoDeVitorias <= 110) {
+
+    switch(true) {
+        case saldoDeVitorias <= 10:
+            rank = "Ferro"
+            break
+        case saldoDeVitorias >= 11 && saldoDeVitorias <= 20:
+            rank = "Bronze"
+            break
+        case saldoDeVitorias >= 21 && saldoDeVitorias <= 50:
+            rank = "Prata"
+            break
+        case saldoDeVitorias >= 51 && saldoDeVitorias <= 80:
+            rank = "Ouro"
+            break
+        case saldoDeVitorias >= 81 && saldoDeVitorias <= 90:
+            rank = "Diamante"
+            break
+        case saldoDeVitorias >= 91 && saldoDeVitorias <= 100:
+            rank = "Lendário"
+            break
+        case saldoDeVitorias >= 101:
+            rank = "Imortal"
+            break
+    }
+
+    console.log(`O Herói tem um saldo de ${saldoDeVitorias} vitórias e está no rank : ${rank}`)
+    saldoDeVitorias += 10
+}
+
+console.log("Achei fácil, como sai do tutorial?")
